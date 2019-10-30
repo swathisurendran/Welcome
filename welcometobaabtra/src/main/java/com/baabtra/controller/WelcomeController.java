14118b3833ab55d487b791ef0f170c63c9cfd578
swathisurendan <swathisurendran26@gmail.com>
2019-10-30 17:06:43 +0530

    initial commit

---
+++ welcometobaabtra/src/main/java/com/baabtra/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.baabtra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")  
+    public String display()  
+    {  
+        return "index";  
+    }     
+
+}
